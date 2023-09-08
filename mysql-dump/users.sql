8cf211bf47dc213a4ec40366fab77d7ce2163baf
Andrew logan <53392022+dev-alt@users.noreply.github.com>
2023-09-08 19:41:07 +1200

    Update users.sql

--- mysql-dump/users.sql
+++ mysql-dump/users.sql
@@ -1,9 +1,9 @@
 -- phpMyAdmin SQL Dump
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
---
+-- SQL DUMP
 -- Host: mysql
--- Generation Time: Aug 21, 2023 at 03:18 AM
+-- Generation Time: Sep 08, 2023 at 07:37 AM
 -- Server version: 8.1.0
 -- PHP Version: 8.2.8
 
@@ -18,50 +18,106 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `Users`
+-- Database: `mydb`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `users`
+-- Table structure for table `UserProfiles`
 --
 
-CREATE TABLE `users` (
+CREATE TABLE `UserProfiles` (
   `id` int NOT NULL,
-  `username` varchar(255) NOT NULL,
-  `email` varchar(255) NOT NULL
+  `bio` text,
+  `dateOfBirth` date DEFAULT NULL,
+  `gender` varchar(255) DEFAULT NULL,
+  `interests` varchar(255) DEFAULT NULL,
+  `socialMediaLinks` json DEFAULT NULL,
+  `address` varchar(255) DEFAULT NULL,
+  `profilePicture` varchar(255) DEFAULT NULL,
+  `website` varchar(255) DEFAULT NULL,
+  `contactEmail` varchar(255) DEFAULT NULL,
+  `contactPhone` varchar(255) DEFAULT NULL,
+  `createdAt` datetime NOT NULL,
+  `updatedAt` datetime NOT NULL,
+  `UserId` int DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
--- Dumping data for table `users`
+-- Dumping data for table `UserProfiles`
 --
 
-INSERT INTO `users` (`id`, `username`, `email`) VALUES
-(1, 'Steve', 'steve@email.com'),
-(2, 'Pete', 'pete@email.com'),
-(3, 'Andrew', 'andy@email.com');
+INSERT INTO `UserProfiles` (`id`, `bio`, `dateOfBirth`, `gender`, `interests`, `socialMediaLinks`, `address`, `profilePicture`, `website`, `contactEmail`, `contactPhone`, `createdAt`, `updatedAt`, `UserId`) VALUES
+(3, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2023-09-08 07:36:26', '2023-09-08 07:36:26', 3);
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Users`
+--
+
+CREATE TABLE `Users` (
+  `id` int NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `firstName` varchar(255) NOT NULL,
+  `lastName` varchar(255) NOT NULL,
+  `createdAt` datetime NOT NULL,
+  `updatedAt` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `Users`
+--
+
+INSERT INTO `Users` (`id`, `username`, `email`, `password`, `firstName`, `lastName`, `createdAt`, `updatedAt`) VALUES
+(3, 'test', 'test@email.com', '$2b$10$0goiLMDfb1KveTL3sxEKUe9LuBGSJy6WdWO51KzVoRxeyPeaNO/Em', 'test', 'user', '2023-09-08 07:36:26', '2023-09-08 07:36:26');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `users`
+-- Indexes for table `UserProfiles`
 --
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
+ALTER TABLE `UserProfiles`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `UserId` (`UserId`);
+
+--
+-- Indexes for table `Users`
+--
+ALTER TABLE `Users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `users`
+-- AUTO_INCREMENT for table `UserProfiles`
 --
-ALTER TABLE `users`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+ALTER TABLE `UserProfiles`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `Users`
+--
+ALTER TABLE `Users`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `UserProfiles`
+--
+ALTER TABLE `UserProfiles`
+  ADD CONSTRAINT `UserProfiles_ibfk_1` FOREIGN KEY (`UserId`) REFERENCES `Users` (`id`) ON DELETE SET NULL ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
