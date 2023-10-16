75eb463ca3e39c6099925dde54a0d3cad1614d62
Andrew <andrew.logan@hotmail.com>
2023-10-17 11:00:52 +1300

    v5

--- Dockerfile
+++ Dockerfile
@@ -3,11 +3,9 @@ FROM node:16
 WORKDIR /app
 
 COPY tradeapp /app/tradeapp
-COPY tradeapp-dashboard /app/tradeapp-dashboard
 COPY tradeapp-backend /app/tradeapp-backend
 
 RUN cd /Tradeapp/tradeapp && npm install
-RUN cd /app/tradeapp-dashboard && npm install
 RUN cd /app/tradeapp-backend && npm install
 
 CMD ["npm", "start"]
--- mysql-dump/users.sql
+++ mysql-dump/users.sql
@@ -1,9 +1,9 @@
 -- phpMyAdmin SQL Dump
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
--- SQL DUMP
+--
 -- Host: mysql
--- Generation Time: Sep 08, 2023 at 07:37 AM
+-- Generation Time: Sep 13, 2023 at 07:08 AM
 -- Server version: 8.1.0
 -- PHP Version: 8.2.8
 
@@ -20,6 +20,74 @@ SET time_zone = "+00:00";
 --
 -- Database: `mydb`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Categories`
+--
+
+CREATE TABLE `Categories` (
+  `id` int NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `createdAt` datetime NOT NULL,
+  `updatedAt` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `Categories`
+--
+
+INSERT INTO `Categories` (`id`, `name`, `createdAt`, `updatedAt`) VALUES
+(1, 'Information Technology', '2023-09-13 07:07:13', '2023-09-13 07:07:13'),
+(2, 'Healthcare', '2023-09-13 07:07:13', '2023-09-13 07:07:13'),
+(3, 'Finance', '2023-09-13 07:07:13', '2023-09-13 07:07:13'),
+(4, 'Engineering', '2023-09-13 07:07:13', '2023-09-13 07:07:13'),
+(5, 'Marketing', '2023-09-13 07:07:13', '2023-09-13 07:07:13'),
+(6, 'Education', '2023-09-13 07:07:13', '2023-09-13 07:07:13'),
+(7, 'Sales', '2023-09-13 07:07:13', '2023-09-13 07:07:13'),
+(8, 'Customer Service', '2023-09-13 07:07:13', '2023-09-13 07:07:13'),
+(9, 'Human Resources', '2023-09-13 07:07:13', '2023-09-13 07:07:13'),
+(10, 'Design', '2023-09-13 07:07:13', '2023-09-13 07:07:13'),
+(11, 'Manufacturing', '2023-09-13 07:07:13', '2023-09-13 07:07:13'),
+(12, 'Legal', '2023-09-13 07:07:13', '2023-09-13 07:07:13'),
+(13, 'Hospitality', '2023-09-13 07:07:13', '2023-09-13 07:07:13'),
+(14, 'Retail', '2023-09-13 07:07:13', '2023-09-13 07:07:13'),
+(15, 'Construction', '2023-09-13 07:07:13', '2023-09-13 07:07:13'),
+(16, 'Transportation', '2023-09-13 07:07:13', '2023-09-13 07:07:13'),
+(17, 'Science', '2023-09-13 07:07:13', '2023-09-13 07:07:13'),
+(18, 'Arts and Entertainment', '2023-09-13 07:07:13', '2023-09-13 07:07:13'),
+(19, 'Non-Profit', '2023-09-13 07:07:13', '2023-09-13 07:07:13'),
+(20, 'Agriculture', '2023-09-13 07:07:13', '2023-09-13 07:07:13');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Jobs`
+--
+
+CREATE TABLE `Jobs` (
+  `id` int NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `description` text NOT NULL,
+  `location` varchar(255) NOT NULL,
+  `deadline` date NOT NULL,
+  `paymentType` varchar(255) NOT NULL,
+  `skillLevel` varchar(255) NOT NULL,
+  `experienceRequired` varchar(255) NOT NULL,
+  `jobStatus` varchar(255) NOT NULL,
+  `createdAt` datetime NOT NULL,
+  `updatedAt` datetime NOT NULL,
+  `user_id` int DEFAULT NULL,
+  `category_id` int DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `Jobs`
+--
+
+INSERT INTO `Jobs` (`id`, `title`, `description`, `location`, `deadline`, `paymentType`, `skillLevel`, `experienceRequired`, `jobStatus`, `createdAt`, `updatedAt`, `user_id`, `category_id`) VALUES
+(5, 'Software Engineer', 'Join our team as a software engineer...', 'New York', '2023-10-01', 'Salary', 'Senior', '5+ years', 'Open', '2023-09-13 07:07:58', '2023-09-13 07:07:58', 3, 1);
 
 -- --------------------------------------------------------
 
@@ -80,6 +148,20 @@ INSERT INTO `Users` (`id`, `username`, `email`, `password`, `firstName`, `lastNa
 --
 
 --
+-- Indexes for table `Categories`
+--
+ALTER TABLE `Categories`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `Jobs`
+--
+ALTER TABLE `Jobs`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `category_id` (`category_id`);
+
+--
 -- Indexes for table `UserProfiles`
 --
 ALTER TABLE `UserProfiles`
@@ -91,11 +173,28 @@ ALTER TABLE `UserProfiles`
 --
 ALTER TABLE `Users`
   ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `email` (`email`);
+  ADD UNIQUE KEY `email` (`email`),
+  ADD UNIQUE KEY `email_2` (`email`),
+  ADD UNIQUE KEY `email_3` (`email`),
+  ADD UNIQUE KEY `email_4` (`email`),
+  ADD UNIQUE KEY `email_5` (`email`),
+  ADD UNIQUE KEY `email_6` (`email`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `Categories`
+--
+ALTER TABLE `Categories`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+
+--
+-- AUTO_INCREMENT for table `Jobs`
+--
+ALTER TABLE `Jobs`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `UserProfiles`
@@ -112,6 +211,13 @@ ALTER TABLE `Users`
 --
 -- Constraints for dumped tables
 --
+
+--
+-- Constraints for table `Jobs`
+--
+ALTER TABLE `Jobs`
+  ADD CONSTRAINT `Jobs_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `Users` (`id`) ON DELETE SET NULL ON UPDATE CASCADE,
+  ADD CONSTRAINT `Jobs_ibfk_2` FOREIGN KEY (`category_id`) REFERENCES `Categories` (`id`) ON DELETE SET NULL ON UPDATE CASCADE;
 
 --
 -- Constraints for table `UserProfiles`
